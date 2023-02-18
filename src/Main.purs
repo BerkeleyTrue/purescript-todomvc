ca7f0e98a5ba0a318779470521c5ccb2ce1f2043
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-02-17 21:26:46 -0700

    feat: add footer, todo list and item

--- src/Main.purs
+++ src/Main.purs
@@ -10,21 +10,52 @@ import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
 
-component :: forall q m. MonadEffect m => H.Component q Unit Unit m
-component = H.mkComponent
+todoItem :: forall props act. HH.HTML props act
+todoItem = HH.li [ HP.classes [ HH.ClassName "todo" ] ]
+  [ HH.div [ HP.classes [ HH.ClassName "view" ] ]
+      [ HH.input [ HP.classes [ HH.ClassName "toggle" ], HP.type_ HP.InputCheckbox ]
+      , HH.label [] [ HH.text "some todo" ]
+      , HH.button [ HP.classes [ HH.ClassName "destroy" ] ] []
+      ]
+  ]
+
+todoList :: forall props act. HH.HTML props act
+todoList = HH.ul [ HP.classes [ HH.ClassName "todo-list" ] ]
+  [ todoItem
+  ]
+
+footer :: forall props act. HH.HTML props act
+footer = HH.footer [ HP.classes [ HH.ClassName "info" ] ]
+  [ HH.p_ [ HH.text "Double-click to edit a todo" ]
+  , HH.p_
+      [ HH.text "created by "
+      , HH.a [ HP.href "https://github.com/berkeleytrue" ] [ HH.text "berkeleytrue" ]
+      ]
+  , HH.p_
+      [ HH.text "Part of "
+      , HH.a [ HP.href "http://todomvc.com" ] [ HH.text "TodoMVC" ]
+      ]
+  ]
+
+appMain :: forall q m. MonadEffect m => H.Component q Unit Unit m
+appMain = H.mkComponent
   { initialState: const unit
   , render
   , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
   }
   where
   handleAction _ = pure unit
-  render _ = HH.div [ HP.classes [ HH.ClassName "todoapp" ] ]
-    [ HH.header [ HP.classes [ HH.ClassName "header" ] ]
-        [ HH.h1 [] [ HH.text "Hello, World!" ]
-        ]
-    ]
+  render _ =
+    HH.div_
+      [ HH.div [ HP.classes [ HH.ClassName "todoapp" ] ]
+          [ HH.header [ HP.classes [ HH.ClassName "header" ] ]
+              [ HH.h1 [] [ HH.text "todos" ] ]
+          ]
+      , todoList
+      , footer
+      ]
 
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  void $ runUI component unit body
+  void $ runUI appMain unit body
