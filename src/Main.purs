1799540515d4e4d9b4c2e802f730bce2a6c9b77e
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-02-17 19:59:43 -0700

    feat: add some structure to component

--- src/Main.purs
+++ src/Main.purs
@@ -5,16 +5,22 @@ import Prelude
 import Effect (Effect)
 import Halogen as H
 import Halogen.Aff as HA
-import Halogen.HTML (text)
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
 
 component = H.mkComponent
   { initialState: const unit
-  , render: \_ -> text "Hello, World!"
+  , render
   , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
   }
   where
-    handleAction _ = pure unit
+  handleAction _ = pure unit
+  render _ = HH.div [ HP.classes [ HH.ClassName "todoapp" ] ]
+    [ HH.header [ HP.classes [ HH.ClassName "header" ] ]
+        [ HH.h1 [] [ HH.text "Hello, World!" ]
+        ]
+    ]
 
 main :: Effect Unit
 main = HA.runHalogenAff do
